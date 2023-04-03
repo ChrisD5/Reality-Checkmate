11ae50f534c18b6d082a8ec087d8b72cead344c0
Chris Dixon <dixonC5@Cardiff.ac.uk>
2023-04-03 16:54:20 +0100

    Created Daily Notifications which can be set based on a certain time and customised. -

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/FirstActivity.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/FirstActivity.java
@@ -1,20 +1,80 @@
 package com.example.mob_dev_portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowInsetsController;
 import com.example.mob_dev_portfolio.databinding.ActivityFirstBinding;
+
+import java.util.Calendar;
 
 public class FirstActivity extends AppCompatActivity {
 
     private ActivityFirstBinding binding;
+    private boolean keep = true;
+    private final int DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         binding = ActivityFirstBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+        setTheme(R.style.Theme_AppCompat_Light);
+        getSupportActionBar().hide();
         setContentView(view);
+
+        splashScreen.setKeepOnScreenCondition(() -> keep);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> keep = false, DELAY);;
+
+        NotificationChannel();
+
+
+        //In order to test notification functionality, change this date to your current date +2 minutes and restart the app then close app in emulator.
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 16);
+        calendar.set(Calendar.MINUTE, 49);
+        calendar.set(Calendar.SECOND, 00);
+
+        if (Calendar.getInstance().after(calendar)) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        Intent intent = new Intent(FirstActivity.this, MemoBroadcast.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+        }
+    }
+
+    private void NotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Reality Check-Mate";
+            String description = "Reality Check-Mate Channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Notification", name, importance);
+            channel.setDescription(description);
+
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+
+        }
     }
 }
+
