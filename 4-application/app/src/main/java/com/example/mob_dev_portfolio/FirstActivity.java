e3b6138663eef4022c3f65192cee417e7e91dc33
Chris Dixon <dixonC5@Cardiff.ac.uk>
2023-04-09 15:58:05 +0100

    Set custom time

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/FirstActivity.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/FirstActivity.java
@@ -42,8 +42,8 @@ public class FirstActivity extends AppCompatActivity {
 
         //In order to test notification functionality, change this date to your current date +2 minutes and restart the app then close app in emulator.
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 16);
-        calendar.set(Calendar.MINUTE, 49);
+        calendar.set(Calendar.HOUR_OF_DAY, 9);
+        calendar.set(Calendar.MINUTE, 30);
         calendar.set(Calendar.SECOND, 00);
 
         if (Calendar.getInstance().after(calendar)) {
